7f1fc06ab5f526e44970f30d99fa27ff6201598b
Md. AL MUHEETU <almuheetu@gmail.com>
2026-04-09 12:09:58 +0600

    Implement GetNotesUseCase to retrieve notes from the repository

--- app/src/main/java/com/shihab/simplenoteapp/domain/usecase/GetNotesUseCase.kt
+++ app/src/main/java/com/shihab/simplenoteapp/domain/usecase/GetNotesUseCase.kt
@@ -1,8 +1,13 @@
 package com.shihab.simplenoteapp.domain.usecase
 
+import com.shihab.simplenoteapp.data.repository.NoteRepository
+import com.shihab.simplenoteapp.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
 class GetNotesUseCase(
-    private val repository:
-
-){
-
+    private val repository: NoteRepository
+) {
+    operator fun invoke() : Flow<List<Note>> {
+        return repository.getNotes()
+    }
 }
