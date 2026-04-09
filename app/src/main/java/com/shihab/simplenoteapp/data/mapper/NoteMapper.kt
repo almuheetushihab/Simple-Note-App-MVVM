a84388f59bc41a9a809af50f6bc9c557ccb190a4
Md. AL MUHEETU <almuheetu@gmail.com>
2026-04-09 14:20:33 +0600

    Add NoteMapper and NoteRepositoryImplement for note entity mapping and repository implementation

--- app/src/main/java/com/shihab/simplenoteapp/data/mapper/NoteMapper.kt
+++ app/src/main/java/com/shihab/simplenoteapp/data/mapper/NoteMapper.kt
@@ -1,2 +1,22 @@
 package com.shihab.simplenoteapp.data.mapper
 
+import com.shihab.simplenoteapp.data.local.NoteEntity
+import com.shihab.simplenoteapp.domain.model.Note
+
+fun NoteEntity.toNote(): Note {
+    return Note(
+        id = id,
+        title = title,
+        content = content,
+        timestamp = timestamp
+    )
+}
+
+fun Note.toNoteEntity(): NoteEntity {
+    return NoteEntity(
+        id = id,
+        title = title,
+        content = content,
+        timestamp = timestamp
+    )
+}
--- app/src/main/java/com/shihab/simplenoteapp/data/repository/NoteRepositoryImplement.kt
+++ app/src/main/java/com/shihab/simplenoteapp/data/repository/NoteRepositoryImplement.kt
@@ -1,2 +1,32 @@
 package com.shihab.simplenoteapp.data.repository
 
+import com.shihab.simplenoteapp.data.local.NoteDao
+import com.shihab.simplenoteapp.data.mapper.toNote
+import com.shihab.simplenoteapp.data.mapper.toNoteEntity
+import com.shihab.simplenoteapp.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class NoteRepositoryImplement (
+    private val dao: NoteDao
+) : NoteRepository {
+
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes().map { entities ->
+            entities.map { it.toNote() }
+        }
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote(note.toNoteEntity())
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note.toNoteEntity())
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        TODO("Not yet implemented")
+    }
+
+}
