8a116adbeebbd95cd6a58492ef0b2e0789c839fd
Md. AL MUHEETU <almuheetu@gmail.com>
2026-04-09 12:19:36 +0600

    Add AddNoteUseCase for inserting notes and handle empty title validation

--- app/src/main/java/com/shihab/simplenoteapp/domain/usecase/AddNoteUseCase.kt
+++ app/src/main/java/com/shihab/simplenoteapp/domain/usecase/AddNoteUseCase.kt
@@ -1,2 +1,17 @@
 package com.shihab.simplenoteapp.domain.usecase
 
+import com.shihab.simplenoteapp.data.repository.NoteRepository
+import com.shihab.simplenoteapp.domain.model.Note
+
+class AddNoteUseCase(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(note: Note) {
+        if (note.title.isBlank()) {
+            throw InvalidNoteException("The title of the note can't be empty.")
+        }
+        repository.insertNote(note)
+    }
+}
+
+class InvalidNoteException(message: String) : Exception(message)
